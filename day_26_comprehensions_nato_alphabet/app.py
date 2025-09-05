4f4d761cf43e35f878cd8ce92ac924881d0e35e4
Javohir_Joniqulov <Javohir200211@gmail.com>
2025-09-06 01:07:02 +0500

    day 30 completed

--- day_26_comprehensions_nato_alphabet/app.py
+++ day_26_comprehensions_nato_alphabet/app.py
@@ -7,5 +7,9 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 def get_letter_code(let):
     return df[df["letter"]==let]["code"].item()
 
-input_text = input("Enter a word: ").strip().upper()
-print([get_letter_code(ch) for ch in input_text])
+while True:
+    input_text = input("Enter a word: ").strip().upper()
+    if input_text.isalpha():
+        print([get_letter_code(ch) for ch in input_text])
+        break
+    print("Sorry, only letters in the alphabet please.")
