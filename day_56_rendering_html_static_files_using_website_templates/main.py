fb1d8016a3c0ab98d2f84d709077bf34cc4c3f59
Javohir_Joniqulov <Javohir200211@gmail.com>
2025-10-01 22:33:20 +0500

    half day complete

---
+++ day_56_rendering_html_static_files_using_website_templates/main.py
@@ -0,0 +1,23 @@
+"""Day 56. Rendering HTML/Static Files, Using Website Templates"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index_page():
+    return render_template("index.html")
+
+
+@app.route("/hobbies")
+def hobbies_page():
+    return render_template("hobbies.html")
+
+
+@app.route("/contact-me")
+def contact_page():
+    return render_template("contact-me.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
